cb145bf9cd66e8b3f26baf092d24cedf5827b7a4
Shivansh <shiv4nsh@gmail.com>
2018-03-28 17:10:27 +0530

    Final Changes to the code

--- internity/hello.py
+++ internity/hello.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask,redirect
+import os
+
 app = Flask(__name__)
 
+portint = int(os.environ['PORT'])
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return redirect("https://goo.gl/3UHty4", code=302)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port=portint)
