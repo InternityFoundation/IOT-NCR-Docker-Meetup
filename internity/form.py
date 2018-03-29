05f760edb0855bfd26383ca3b6ba8913bf66a2cc
Shivansh <shiv4nsh@gmail.com>
2018-03-29 08:31:35 +0530

    Added form.py

--- internity/form.py
+++ internity/form.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 portint = int(os.environ['PORT'])
 @app.route('/')
 def hello_world():
-    return 'Hello World !'
+    return redirect("https://goo.gl/3UHty4", code=302)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=portint)
